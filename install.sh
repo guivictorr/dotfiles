aa8e966743eb21baaaa4587a2add8354ae73f317
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2022-07-09 22:30:10 -0300

    adding vscode settings files

--- install.sh
+++ install.sh
@@ -12,3 +12,9 @@ ln -s -f $PWD/nvim/init.vim ~/.config/nvim/init.vim
 ln -s -f $PWD/nvim/coc-settings.json ~/.config/nvim/coc-settings.json
 ln -s -f $PWD/nvim/coc-package.json ~/.config/coc/extensions/package.json
 ln -s -f $PWD/nvim/coc-db.json ~/.config/coc/extensions/db.json
+
+# link vscode files
+ln -s -f $PWD/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -s -f $PWD/vscode/settings.json ~/Library/Application\ Support/Code/User/keybindings.json
+
+
