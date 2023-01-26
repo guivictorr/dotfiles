9ac1b3742ea69c6dd9308b06d3de52d9acc1477f
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-01-26 15:23:48 -0300

    changing init.lua to init.vim

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -s -f $PWD/env/.zshrc ~/
 
 # link nvim files
 mkdir -p ~/.config/nvim
-ln -s -f $PWD/nvim/init.lua ~/.config/nvim/init.lua
+ln -s -f $PWD/nvim/init.vim ~/.config/nvim/init.vim
 
 # link vscode files
 ln -s -f $PWD/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
---
+++ nvim/init.vim
@@ -0,0 +1,8 @@
+call plug#begin('~/.config/nvim/plugged')
+
+Plug 'nvim-telescope/telescope.nvim'
+Plug 'nvim-lua/plenary.nvim'
+
+call plug#end()
+
+
