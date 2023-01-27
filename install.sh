3c510f4452b6ba95778a03749e85cf29b6877be6
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-01-27 14:40:01 -0300

    refactoring install.sh and adding mason config

--- install.sh
+++ install.sh
@@ -5,13 +5,16 @@ ln -s -f $PWD/env/.gitconfig ~/
 ln -s -f $PWD/env/.zshrc ~/
 
 # link nvim files
-mkdir -p ~/.config/nvim
-mkdir -p ~/.config/nvim/lua
-mkdir -p ~/.config/nvim/after/plugin
-ln -s -f $PWD/nvim/init.lua ~/.config/nvim/init.lua
-ln -s -f $PWD/nvim/lua/plugins.lua ~/.config/nvim/lua/plugins.lua
-ln -s -f $PWD/nvim/lua/base.lua ~/.config/nvim/lua/base.lua
-ln -s -f $PWD/nvim/after/plugin/lualine.rc.lua ~/.config/nvim/after/plugin/lualine.rc.lua
+dirs=(nvim nvim/lua nvim/plugin nvim/after/plugin)
+
+for dir in "${dirs[@]}"; do
+  mkdir -p ~/.config/$dir
+  for file in $PWD/$dir/*; do
+    if [ ! -d "$file" ]; then
+      ln -s -f $file ~/.config/$dir/
+    fi
+  done
+done
 
 # link vscode files
 ln -s -f $PWD/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
