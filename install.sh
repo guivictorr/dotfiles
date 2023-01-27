84a664755102512c325b3b246eee24e2e147dd84
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-01-27 03:50:46 -0300

    changing neovim config to .lua

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -s -f $PWD/env/.zshrc ~/
 
 # link nvim files
 mkdir -p ~/.config/nvim
-ln -s -f $PWD/nvim/init.vim ~/.config/nvim/init.vim
+ln -s -f $PWD/nvim/init.lua ~/.config/nvim/init.lua
 
 # link vscode files
 ln -s -f $PWD/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
--- nvim/init.vim
+++
@@ -1,8 +0,0 @@
-call plug#begin('~/.config/nvim/plugged')
-
-Plug 'nvim-telescope/telescope.nvim'
-Plug 'nvim-lua/plenary.nvim'
-
-call plug#end()
-
-
