fef3c664eff6506283127246f354427c9e296719
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-04-20 20:26:17 -0300

    updating neovim plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -12,42 +12,50 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-	'manzeloth/live-server',
-	'guivictorr/no-clown-fiesta.nvim',
-	'jose-elias-alvarez/null-ls.nvim',
+	'manzeloth/live-server',          -- To run simple html projects
+	'guivictorr/no-clown-fiesta.nvim', -- Theme
+	'jose-elias-alvarez/null-ls.nvim', -- Formatting and Diagnostic
 	'nvim-lua/plenary.nvim',
 	{
-		"williamboman/mason.nvim",
-		build = ":MasonUpdate" -- :MasonUpdate updates registry contents
+		"williamboman/mason.nvim", -- Portable package manager
+		build = ":MasonUpdate"   -- :MasonUpdate updates registry contents
 	},
 	'williamboman/mason-lspconfig.nvim',
-	'onsails/lspkind-nvim',
+	'onsails/lspkind-nvim', -- Pictograms
+	'hrsh7th/nvim-cmp',    -- Autocomplete
 	'hrsh7th/cmp-buffer',
 	'hrsh7th/cmp-nvim-lsp',
-	'hrsh7th/nvim-cmp',
-	'neovim/nvim-lspconfig',
-	'glepnir/lspsaga.nvim',
+	'neovim/nvim-lspconfig', -- Language server
+	{
+		'glepnir/lspsaga.nvim',
+		event = 'LspAttach',
+		dependencies = {
+			{ "nvim-tree/nvim-web-devicons" },
+			--Please make sure you install markdown and markdown_inline parser
+			{ "nvim-treesitter/nvim-treesitter" }
+		}
+	},
 	'L3MON4D3/LuaSnip',
 	'windwp/nvim-autopairs',
 	'norcalli/nvim-colorizer.lua',
-	'kyazdani42/nvim-web-devicons',
-	'lewis6991/gitsigns.nvim',
-	'akinsho/nvim-bufferline.lua',
+	'nvim-tree/nvim-web-devicons',
+	'lewis6991/gitsigns.nvim',    -- Git signs
+	'akinsho/nvim-bufferline.lua', -- Tabs
 	'windwp/nvim-ts-autotag',
 	{
-		'nvim-treesitter/nvim-treesitter',
+		'nvim-treesitter/nvim-treesitter', -- Code highlight
 		opt = true,
 		run = ':TSUpdate',
 		event = "BufRead",
 	},
-	'nvim-telescope/telescope.nvim',
-	'nvim-telescope/telescope-file-browser.nvim',
+	'nvim-telescope/telescope.nvim',             -- File finder
+	'nvim-telescope/telescope-file-browser.nvim', -- File browser
 	{
-		'nvim-lualine/lualine.nvim',
-		requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+		'nvim-lualine/lualine.nvim',               -- Statusline
+		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 	},
 	{
-		"startup-nvim/startup.nvim",
+		"startup-nvim/startup.nvim", -- Startup screen
 		requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
 	}
 })
--- nvim/plugin/lspsaga.rc.lua
+++ nvim/plugin/lspsaga.rc.lua
@@ -17,10 +17,12 @@ saga.setup({
 local diagnostic = require("lspsaga.diagnostic")
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
+vim.keymap.set('n', '<C-k>', '<Cmd>Lspsaga diagnostic_jump_prev<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('n', 'gd', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+vim.keymap.set('n', 'gd', '<Cmd>Lspsaga goto_definition<CR>', opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga peek_definition<CR>', opts)
 vim.keymap.set('n', 'gr', '<Cmd>Lspsaga rename<CR>', opts)
+vim.keymap.set('n', 'gf', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 
 -- code action
 local codeaction = require("lspsaga.codeaction")
