c3f4c724493900320d9a7bddc0039a74a435cdde
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-05-12 16:47:07 -0300

    testing a different plugin structure

---
+++ nvim/lua/plugins/autotag.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		'windwp/nvim-autopairs',
+		opts = {
+			disable_filetype = { "TelescopePrompt", "vim" },
+		}
+	},
+	'windwp/nvim-ts-autotag',
+}
---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		'rebelot/kanagawa.nvim',
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require('kanagawa').setup {
+				colors = {
+					theme = { all = { ui = { bg_gutter = 'none' } } }
+				}
+			}
+			vim.cmd [[colorscheme kanagawa]]
+		end,
+	}
+}
