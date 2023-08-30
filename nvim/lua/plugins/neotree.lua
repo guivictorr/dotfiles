91d5995a5a48b9c2e6258dba7b6d8406a16989ee
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-08-30 02:43:23 -0300

    showing dotfiles and gitignored files

--- nvim/lua/plugins/neotree.lua
+++ nvim/lua/plugins/neotree.lua
@@ -4,4 +4,12 @@ return {
     { "<leader>e", ":Neotree toggle float<CR>", silent = true, desc = "Float File Explorer" },
     { "<leader>fe", ":Neotree toggle left<CR>", silent = true, desc = "Left File Explorer" },
   },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
+    },
+  },
 }
