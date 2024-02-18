f9f1ca5e4b809e6c6f0fdd329e9f2136f3b3e37a
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-02-17 23:18:18 -0300

    improving the install.sh

--- install.sh
+++ install.sh
@@ -1,24 +1,69 @@
-# git
-ln -s ~/dotfiles/env/.gitconfig ~/
+# Define variables for source and destination directories
+SOURCE_DIR=$HOME/dotfiles
+DESTINATION_DIR=$HOME
 
-# zsh
-ln -s ~/dotfiles/env/.zshrc ~/
-ln -s ~/dotfiles/env/.spaceshiprc.zsh ~/
+create_symlink() {
+	local source=$1
+	local target=$2
 
-# tmux
-ln -s ~/dotfiles/tmux/.tmux.conf ~/
+	# Check if target file already exists
+	if [ ! -e "$target" ]; then
+		# Create symbolic link
+		ln -s "$source" "$target"
+	fi
+}
 
-# kitty
-ln -s ~/dotfiles/kitty/kitty.conf ~/.config/kitty/
+# Function to create symbolic links for files in a directory
+create_symlinks_in_directory() {
+    local source_dir=$1
+    local target_dir=$2
 
-# link nvim files
-dirs=(nvim nvim/lua nvim/lua/plugins nvim/lua/config)
+    # Check if the source directory exists
+    if [ -d "$source_dir" ]; then
+        # Create target directory if it doesn't exist
+        if [ ! -d "$target_dir" ]; then
+            mkdir -p "$target_dir"
+        fi
 
-for dir in "${dirs[@]}"; do
-	mkdir -p ~/.config/$dir
-	for file in ~/dotfiles/$dir/*; do
-		if [ ! -d "$file" ]; then
-			ln -s -f $file ~/.config/$dir/
-		fi
-	done
-done
+        # Iterate through files in source directory
+        for file in "$source_dir"/*; do
+            if [ -f "$file" ]; then
+                # Get file name without path
+                filename=$(basename "$file")
+                # Check if target file already exists
+                if [ ! -e "$target_dir/$filename" ]; then
+                    # Create symbolic link for each file
+                    create_symlink "$file" "$target_dir/$filename"
+                fi
+            elif [ -d "$file" ]; then
+                # Recursively call the function for subdirectories
+                subdir_target_dir="$target_dir/$(basename "$file")"
+                create_symlinks_in_directory "$file" "$subdir_target_dir"
+            fi
+        done
+    else
+        echo "Source directory $source_dir does not exist."
+    fi
+}
+
+# Create symbolic links for each file/folder
+
+# kitty folder
+create_symlinks_in_directory "$SOURCE_DIR/kitty" "$DESTINATION_DIR/.config/kitty"
+
+# nvim folder
+create_symlinks_in_directory "$SOURCE_DIR/nvim" "$DESTINATION_DIR/.config/nvim"
+
+# tmux configuration file
+create_symlinks_in_directory "$SOURCE_DIR/tmux" "$DESTINATION_DIR/.config/tmux"
+
+# .spaceshiprc file
+create_symlink "$SOURCE_DIR/env/spaceship.zsh" "$DESTINATION_DIR/.config/spaceship.zsh"
+
+# .gitconfig file
+create_symlink "$SOURCE_DIR/env/.gitconfig" "$DESTINATION_DIR/.gitconfig"
+
+# .zshrc file
+create_symlink "$SOURCE_DIR/env/.zshrc" "$DESTINATION_DIR/.zshrc"
+
+echo "Installation completed."
