2946b91fb24eb0bc819c5d91b251fb6d907307c4
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-04-28 02:35:27 -0300

    updating install.sh to install tpm

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Check if stow is installed
+# install stow if isn't installed
 if ! command -v stow &>/dev/null; then
 	echo "stow not found, installing..."
 	# Install stow using Homebrew
@@ -8,14 +8,22 @@ if ! command -v stow &>/dev/null; then
 	ln -s $HOME/dotfiles/.stow-global-ignore ~/.stow-global-ignore
 fi
 
+# install bpytop if isn't installed
 if ! command -v bpytop &>/dev/null; then
 	echo "bpytop not found, installing..."
 	brew install bpytop
 fi
 
+# install lazygit if isn't installed
 if ! command -v lazygit &>/dev/null; then
 	echo "lazygit not found, installing..."
 	brew install lazygit
+fi
+
+# install tpm if isn't installed
+if [ ! -d "$HOME/.tmux/plugins/tpm" ]; then
+	echo "tpm not found, installing..."
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 fi
 
 echo "Linking files..."
