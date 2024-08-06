b0cc70c1c491eef4c6c79bef1de6ae011ba0ba03
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-08-05 21:20:43 -0300

    filtering anoying message from noice

--- config/nvim/lua/plugins/noice.lua
+++ config/nvim/lua/plugins/noice.lua
@@ -1,6 +1,17 @@
 return {
   "folke/noice.nvim",
   opts = {
+    routes = {
+      {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = {
+          skip = true,
+        },
+      },
+    },
     cmdline = {
       view = "cmdline",
     },
