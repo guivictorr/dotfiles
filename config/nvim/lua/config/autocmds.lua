d40c3e24e33604feb0149e23047d05ca0349fcfa
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2025-01-12 01:12:02 -0300

    removes padding from kitty when execute BufEnter

--- config/nvim/lua/config/autocmds.lua
+++ config/nvim/lua/config/autocmds.lua
@@ -11,3 +11,21 @@ vim.api.nvim_set_hl(0, "FloatTitle", { link = "SnacksWinBar" })
 vim.api.nvim_set_hl(0, "LspInfoBorder", { link = "FzfLuaBorder" })
 vim.api.nvim_set_hl(0, "FloatBorder", { link = "FzfLuaBorder" })
 vim.api.nvim_set_hl(0, "NormalFloat", { link = "Normal" })
+
+-- Set padding to zero on VimEnter
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  callback = function()
+    vim.cmd(
+      "silent !kitty @ --to unix:$(ls /tmp/mykitty* 2>/dev/null || echo '') set-spacing padding-left=0 padding-top=0 padding-bottom=0 padding-right=0"
+    )
+  end,
+})
+
+-- Restore default padding on VimLeave
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+  callback = function()
+    vim.cmd(
+      "silent !kitty @ --to unix:$(ls /tmp/mykitty* 2>/dev/null || echo '') set-spacing padding-left=default padding-top=default padding-right=default padding-bottom=default"
+    )
+  end,
+})
